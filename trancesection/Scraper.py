6f209db147134ddfcf4b6ea5fd7fd7f8d339ada1
Michel-Minh Nguyen-Quoc <ngq.mmichel@gmail.com>
2014-01-12 14:44:14 -0800

    Added Scraper superclass, abgtScraper subclass

---
+++ trancesection/Scraper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+
+# scraper superclass
+
+class Scraper(object):
+    # base is url without episode number
+    def __init__(self, base):
+        self.base = base
+
+    def getTracks(self, url):
+        raise NotImplementedError
+
+    def scrape(self, num):
+        if type(num) == int:
+            num = str(num)
+        url = base + num
+        return getTracks(url)
